dd1af082e29fb46afbb1c4fd976dc650e276e7f6
Tanase Sergiu <tanasesergiu15@gmail.com>
2026-04-25 21:08:07 +0300

    create session schema:  SessionCreate and Session Response

--- app/schemas/session.py
+++ app/schemas/session.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
-class Session(BaseModel):
+class SessionCreate(BaseModel):
     topic: str
+
+
+class SessionResponse(BaseModel):
+    id: int
+    topic: str
+    status: str
+    user_id: int
+    timestamp: datetime
+
+    class Config:
+        from_attributes = True
