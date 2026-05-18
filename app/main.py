b5130710a86ea0f017c645940649ea8e3932a4fb
Tanase Sergiu <tanasesergiu15@gmail.com>
2026-05-18 11:30:35 +0300

    Login Page has functionality now and update CORS middleware configuration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.routers import auth, sessions, history
 from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router)
 app.include_router(sessions.router)
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 function LoginPage(){
-    const[email,setEmail] = useState("")
-    const[password, setPassword] = useState("")
-    const[error,setError] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
     const navigate = useNavigate()
 
     const handleLogin = async () => {
         try{
-            const response = await fetch("http://localhost:8000/auth/login", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
                 method: "POST",
                 headers: {"Content-Type": "application/x-www-form-urlencoded"},
                 body: new URLSearchParams({username: email, password: password})
@@ -21,33 +21,27 @@ function LoginPage(){
             if(response.ok){
                 localStorage.setItem("token", data.access_token)
                 navigate("/dashboard")
-            } else {
-                setError(data.detail)
+            } else{
+                if (typeof data.detail === "string") {
+                    setError(data.detail)
+                } else {
+                    setError("Invalid email or password")
+                }
             }
-        } catch(err){
+        } catch(err) {
             setError("Cannot connect to server")
         }
     }
 
-    return(
-        <div>
-            <h1>Login</h1>
-            <input
-                type='email'
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>Login</button>
-            {error && <p>{error}</p>}
-        </div>
-    )
+     return(
+            <div>
+                <h1>Login</h1>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={handleLogin}>Login</button>
+                {error && <p>{error}</p>}
+            </div>
+        )
 }
 
 export default LoginPage
